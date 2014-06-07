31efb2c04143b1fc43352c0410d526e62eb1ff15
James Hulley <james.hulley@gmail.com>
2014-06-07 16:56:38 -0700

    Removed json render from activity controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,8 +1,8 @@
 class ActivitiesController < ApplicationController
 
   def index
-    foursqare_api_call = FourSquare::Activitycall.new({params[:long_and_lat]})
-    render :json foursquare.activities.to_json
+    # foursqare_api_call = FourSquare::Activitycall.new({params[:long_and_lat]})
+    # render :json foursquare.activities.to_json
   end
 
 
