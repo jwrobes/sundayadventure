42d9bb7aa16b49a34eaf44ce507a475d59e59e61
Kenneth & Eiko Seino <pair+cpkenn09y.smileyface525@devbootcamp.com>
2014-06-07 12:04:11 -0700

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sundayadventure::Application.config.secret_token = '635157c4c89c9a25d3bb594f21f51053844b1a61b33b909e3e43447e9965e5eaefe031914e145ecbf63ae46918d60f91bff03ea5253b1a4e96b597bb48da63dd'
